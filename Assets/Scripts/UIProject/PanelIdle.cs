0ec97846ea398df9f08d473ba57617643ede77fe
watanabe@marida <watanabe.everystudio@gmail.com>
2024-08-20 15:11:13 +0900

    UIのアニメーションを追加

--- Assets/Scripts/UIProject/PanelIdle.cs
+++ Assets/Scripts/UIProject/PanelIdle.cs
@@ -18,16 +18,36 @@ public class PanelIdle : UIPanel
     public List<TabModel> tabModels = new List<TabModel>();
     [SerializeField] private PanelIdleTab tabPrefab;
 
+    private Animator animator;
+
     protected override void initialize()
     {
+        tabPrefab.gameObject.SetActive(false);
         foreach (var tabModel in tabModels)
         {
             var tab = Instantiate(tabPrefab, tabRoot);
+            tab.gameObject.SetActive(true);
             tab.Initialize(tabModel);
             tab.OnSelected += OnTabSelected;
         }
-
+        animator = GetComponent<Animator>();
     }
+
+    public void Open(bool isOpen)
+    {
+        Debug.Log($"Open:{isOpen}");
+        animator.SetBool("isOpen", isOpen);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Open(false);
+        }
+    }
+
+
 
     private void OnTabSelected(TabModel model)
     {
